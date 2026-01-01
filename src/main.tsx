40cbf11d3715806dc1cb9cdac5c93fe789ff9e8e
CyberSphinxx <johnlemargonzales@gmail.com>
2026-01-02 00:09:39 +0800

    feat: Wrap app with AuthProvider for authentication context

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { UserAuthContextProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <UserAuthContextProvider>
+      <App />
+    </UserAuthContextProvider>
   </StrictMode>,
 )
