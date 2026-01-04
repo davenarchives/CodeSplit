d7f9396d25e4596b250dc0578cc40d4da569fd9b
CyberSphinxx <johnlemargonzales@gmail.com>
2026-01-05 00:26:43 +0800

    Add ToastProvider to application root

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import { UserAuthContextProvider } from './context/AuthContext'
+import { ToastProvider } from './context/ToastContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <UserAuthContextProvider>
-        <App />
-      </UserAuthContextProvider>
+      <ToastProvider>
+        <UserAuthContextProvider>
+          <App />
+        </UserAuthContextProvider>
+      </ToastProvider>
     </BrowserRouter>
   </StrictMode>,
 )
